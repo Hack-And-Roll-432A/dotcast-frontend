f57b8b9429239268430a80d3a11ed43e7f7bb258
LouisLouis19 <andreaslouis.louis@gmail.com>
2023-01-14 19:53:54 +0800

    complete components for home page, minus styling

--- dotcast/src/views/HomeView.vue
+++ dotcast/src/views/HomeView.vue
@@ -15,20 +15,57 @@
       ></v-select>
     </div>
 
-    <div class="categories"></div>
+    <div class="categories">
+      <div
+        class="check-options bg-gray-300 inset-0 absolute flex flex-col items-center justify-center"
+      >
+        <multi-check-box v-model:value="categories" :options="options" />
+      </div>
+    </div>
+
+    <div class="generate">
+      <button>
+        <p>generate</p>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import vSelect from "vue-select";
+import MultiCheckbox from "../components/multi-checkbox.vue";
+import { ref, onMounted } from "vue";
+
 export default {
   data() {
     return {
       mrtstation: ["Kent Ridge", "Buona Vista"],
     };
   },
+  setup() {
+    let categories = ref([]);
+    let options = ref([]);
+
+    const getCategoryOptions = () => {
+      options.value = [
+        { name: "Sports", id: 1 },
+        { name: "Rock", id: 2 },
+        { name: "Fantasy", id: 3 },
+      ];
+    };
+
+    onMounted(() => {
+      getCategoryOptions();
+    });
+
+    return {
+      categories,
+      options,
+    };
+  },
   components: {
     vSelect,
+    "multi-check-box": MultiCheckbox,
   },
 };
 </script>
@@ -46,5 +83,15 @@ export default {
 .vs__actions {
   display: none;
   visibility: hidden;
+}
+
+.categories {
+  display: flex;
+  flex-direction: column;
+}
+
+.check-options {
+  display: flex;
+  flex-direction: column;
 }
 </style>
